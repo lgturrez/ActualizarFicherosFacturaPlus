39b1edc4dd94541f0ff8247379c75caa9ab191d0
lgturrez <Infousuario2@gmail.com>
2020-02-14 11:05:57 +0100

    Add files via upload

---
+++ src/ActualizarFicherosFacturaPlus.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnSimular = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(24, 71);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "Actualizar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ubicación de los ficheros";
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Enabled = false;
+            this.txtRuta.Location = new System.Drawing.Point(24, 34);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(312, 20);
+            this.txtRuta.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 326);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(388, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(342, 34);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(25, 20);
+            this.btnOpenFile.TabIndex = 4;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(30, 116);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(115, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "                                    ";
+            // 
+            // btnSimular
+            // 
+            this.btnSimular.Location = new System.Drawing.Point(292, 71);
+            this.btnSimular.Name = "btnSimular";
+            this.btnSimular.Size = new System.Drawing.Size(75, 23);
+            this.btnSimular.TabIndex = 6;
+            this.btnSimular.Text = "Simular";
+            this.btnSimular.UseVisualStyleBackColor = true;
+            this.btnSimular.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 348);
+            this.Controls.Add(this.btnSimular);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtRuta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnSimular;
+    }
+}
+
---
+++ src/ActualizarFicherosFacturaPlus.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<string> lsFileList = new List<string>();
+        private string driver;
+        private Boolean blnSimular = false;
+        private Boolean blnPreguntar = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ejecutar()
+        {
+            if (!blnPreguntar || MessageBox.Show("Se van a sobreescribir los ficheros de FacturaPlus. " + Environment.NewLine + "Asegúrate de haber hecho una copia de seguridad. " + Environment.NewLine + "¿Continuar?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                foreach (string fichero in lsFileList)
+                {
+                    /*if (driver.Equals("MSOLEDB")){
+                        UpdateData(txtRuta.Text, fichero, ConfigurationSettings.AppSettings[fichero + "_PK"]);
+                    }else
+                    {
+                        UpdateData1(txtRuta.Text, fichero, ConfigurationSettings.AppSettings[fichero + "_PK"]);
+                    }*/
+                    UpdateData1(txtRuta.Text, fichero, ConfigurationSettings.AppSettings[fichero + "_PK"]);
+                }
+                toolStripStatusLabel1.Text = "Finalizado";
+            }else
+            {
+                toolStripStatusLabel1.Text = "Proceso cancelado";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "";
+            blnPreguntar = true;
+            blnSimular = false;
+            ejecutar();
+            
+        }
+        
+        public Boolean UpdateData(String dbLocation, String tablename, String PK)
+        {
+            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dbLocation + ";Extended Properties=dBASE IV;User ID=;Password=;"); // give your path directly 
+            try
+            {
+                int conta = 0;
+                if (PK == null) PK = ConfigurationSettings.AppSettings["default_PK"];
+                con.Open();
+                toolStripStatusLabel1.Text = "Actualizando tabla " + tablename;
+
+                OleDbDataAdapter da = new OleDbDataAdapter("select * from " + tablename + " order by " + PK , con); // update this query with your table name 
+
+                int contador = 1;
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                DataRow fila0 = ds.Tables[0].Rows[0];
+
+                int totalRows = ds.Tables[0].Rows.Count;
+
+                List<String> arrColumnasFecha = new List<String>();
+                foreach (DataColumn column in ds.Tables[0].Columns)
+                {
+                    if (fila0[column.ColumnName.ToString()].GetType() == typeof(DateTime))
+                    {
+                        arrColumnasFecha.Add(column.ColumnName.ToString());
+                    }
+                }
+
+                foreach (DataRow  item in ds.Tables[0].Rows)
+                {
+                    toolStripStatusLabel1.Text = "Actualizando tabla " + tablename + " - Registro " + contador + " de " + totalRows;
+                    statusStrip1.Refresh();
+                    Boolean blnCambiado = false;
+                    foreach (String columna in arrColumnasFecha)
+                    {
+                        DateTime temp = (DateTime)item[columna];
+                        if (temp.Year == 1920)
+                        {
+                            item[columna] = temp.AddYears(100);
+                            OleDbCommand cmd = new OleDbCommand("UPDATE " + tablename + " SET " + columna + " = '" + ((DateTime)item[columna]).ToString("MM/dd/yyyy") + "' WHERE " + PK + " = " + item[PK] + ";", con);
+                            cmd.ExecuteNonQuery();
+                            blnCambiado = true;
+                            
+                        }
+                    }
+                    if (blnCambiado) conta++;
+                    contador++;
+                        
+                }
+                
+                con.Close();
+                lblResultado.Text = lblResultado.Text + Environment.NewLine + tablename + ": " + conta + " registros actualizados de " + totalRows;
+                lblResultado.Refresh();
+                conta = 0;
+                toolStripStatusLabel1.Text = "Finalizado";
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                var error = e.ToString();
+                // check error details 
+                return false;
+            }
+        }
+
+        public Boolean UpdateData1(String dbLocation, String tablename, String PK)
+        {
+            OleDbConnection con;
+            try
+            {
+
+                if (driver.Equals("MSOLEDB"))
+                {
+                    con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dbLocation + ";Extended Properties=dBASE IV;User ID=;Password=;"); // give your path directly 
+                    con.Open();
+                }
+                else
+                {
+                    con = new OleDbConnection(@"Provider=VFPOLEDB;Data Source=" + dbLocation + ";Extended Properties=dBASE IV;User ID=;Password=;"); // give your path directly 
+                    con.Open();
+                    OleDbCommand cmd = new OleDbCommand("SET DELETED OFF", con);
+                    cmd.ExecuteNonQuery();
+                }
+
+                int conta = 0;
+                if (PK == null) PK = ConfigurationSettings.AppSettings["default_PK"];
+                
+                toolStripStatusLabel1.Text = "Actualizando tabla " + tablename;
+
+                OleDbDataAdapter da = new OleDbDataAdapter("select * from " + tablename  + " order by " + PK, con); // update this query with your table name 
+                //OleDbDataAdapter da = new OleDbDataAdapter("select * from " + tablename +" where " + PK + "= 60 order by " + PK, con); // update this query with your table name 
+
+                int contador = 1;
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                DataRow fila0 = ds.Tables[0].Rows[0];
+
+                int totalRows = ds.Tables[0].Rows.Count;
+
+                List<String> arrColumnasFecha = new List<String>();
+                foreach (DataColumn column in ds.Tables[0].Columns)
+                {
+                    if (fila0[column.ColumnName.ToString()].GetType() == typeof(DateTime))
+                    {
+                        arrColumnasFecha.Add(column.ColumnName.ToString());
+                    }
+                }
+
+                foreach (DataRow item in ds.Tables[0].Rows)
+                {
+                    toolStripStatusLabel1.Text = "Actualizando tabla " + tablename + " - Registro " + contador + " de " + totalRows;
+                    statusStrip1.Refresh();
+                    Boolean blnCambiado = false;
+                    foreach (String columna in arrColumnasFecha)
+                    {
+                        DateTime temp = (DateTime)item[columna];
+                        if (temp.Year >= 1920 && temp.Year <= 1930)
+                        {
+                            try
+                            {
+                                item[columna] = temp.AddYears(100);
+                                string sentenciaSQL;
+                                if (driver.Equals("MSOLEDB"))
+                                {
+                                    sentenciaSQL = "UPDATE " + tablename + " SET " + columna + " = '" + ((DateTime)item[columna]).ToString("dd/MM/yyyy") + "' WHERE " + PK + " = " + item[PK] + ";"; 
+                                }else
+                                {
+                                    sentenciaSQL = "UPDATE " + tablename + " SET " + columna + " = {" + ((DateTime)item[columna]).ToString("MM/dd/yyyy") + "} WHERE " + PK + " = " + item[PK] + ";";
+                                }
+                                OleDbCommand cmd = new OleDbCommand(sentenciaSQL, con);
+                                if (!blnSimular)
+                                {
+                                    int rows = cmd.ExecuteNonQuery();
+                                }
+                                //Console.WriteLine(sentenciaSQL + " - Registros: " + rows);
+                                blnCambiado = true;
+                            }
+                            catch (Exception e)
+                            {
+                                var error = e.ToString();
+                                Console.WriteLine (error);
+                                
+                            }
+
+                        }else
+                        {
+                            //Console.WriteLine("No es 1920");
+                        }
+                    }
+                    if (blnCambiado) conta++;
+                    contador++;
+
+                }
+                
+                con.Close();
+                lblResultado.Text = lblResultado.Text + Environment.NewLine + tablename + ": " + conta + " registros actualizados de " + totalRows;
+                lblResultado.Refresh();
+                conta = 0;
+                toolStripStatusLabel1.Text = "Finalizado";
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                var error = e.ToString();
+                // check error details 
+                return false;
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog();
+        }
+
+        private void OpenFileDialog()
+        {
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+            folderBrowserDialog1.SelectedPath = txtRuta.Text;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtRuta.Text = folderBrowserDialog1.SelectedPath;
+                CargarFicheros();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            Boolean blnAutorun = false;
+
+            foreach (string arg in args)
+            {
+                if (arg.Equals ("autorun")){
+                    blnAutorun = true;        
+                }
+            }
+
+            txtRuta.Text = ConfigurationSettings.AppSettings["carpetaInicial"];  //System.Configuration.ConfigurationSettings.AppSettings["carpetaInicial"];
+            CargarFicheros();
+            driver = ConfigurationSettings.AppSettings["driver"];
+            if (blnAutorun)
+            {
+                blnPreguntar = false;
+                ejecutar();
+                Application.Exit();
+            }
+        }
+
+        private static string[] GetFileNames(string path, string filter)
+        {
+            string[] files = Directory.GetFiles(path, filter,SearchOption.TopDirectoryOnly);
+            for (int i = 0; i < files.Length; i++)
+                files[i] = Path.GetFileName(files[i]);
+            return files;
+        }
+
+        private void CargarFicheros()
+        {
+            if (Directory.Exists(txtRuta.Text)){
+                string ficheros = ConfigurationSettings.AppSettings["Ficheros"];
+                if (ficheros.Equals("*"))
+                {
+                    lsFileList = new List<string>(GetFileNames(txtRuta.Text, "*.dbf"));
+                }
+                else
+                {
+                    lsFileList = new List<string>(ficheros.Split('#'));
+                }
+                if (lsFileList.Count == 0)
+                {
+                    MessageBox.Show("No hay ficheros .dbf en el directorio", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    btnUpdate.Enabled = false;
+                }
+                else
+                {
+                    btnUpdate.Enabled = true;
+                }
+                                
+            }
+            else
+            {
+                MessageBox.Show("El directorio no existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                OpenFileDialog();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            blnSimular = true;
+            blnPreguntar = false;
+            lblResultado.Text = "ESTO ES UNA SIMULACION, LOS FICHEROS NO SE MODIFICAN";
+            ejecutar();
+            blnSimular = false;
+        }
+    }
+}
